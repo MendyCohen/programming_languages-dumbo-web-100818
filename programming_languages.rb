45e467f69c4bb321ad7871abccdbbc4b3035b192
Mendy <mendybeltcohen@gmail.com>
2018-09-26 22:59:56 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.collect do |language_hash, inside_language_hash|
+    inside_language_hash.collect do |language, inside_language|
+      if !new_hash[language]
+        new_hash[language] = inside_language
+        new_hash[language].merge!({style: [language_hash]})
+      else 
+        new_hash[language][:style] << language_hash
+      end
+    end
+  end
+  new_hash
 end
